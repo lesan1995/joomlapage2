ef4960c460887cb1e1319a1b5734f6f717cf33f5
lesan1995 <lesan1995@gmail.com>
2018-04-08 21:06:28 +0700

    first commit

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-02-24 08:55:26 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-02-24T08:55:26+00:00	INFO ::1	update	Update started by user Super User (96). Old version is 3.8.4.
+2018-02-24T08:55:27+00:00	INFO ::1	update	Downloading update file from https://downloads.joomla.org/cms/joomla3/3-8-5/Joomla_3.8.5-Stable-Update_Package.zip.
+2018-02-24T08:55:44+00:00	INFO ::1	update	File Joomla_3.8.5-Stable-Update_Package.zip downloaded.
+2018-02-24T08:55:45+00:00	INFO ::1	update	Starting installation of new version.
+2018-02-24T09:05:18+00:00	INFO ::1	update	Finalising installation.
+2018-02-24T09:05:18+00:00	INFO ::1	update	Deleting removed files and folders.
+2018-02-24T09:05:23+00:00	INFO ::1	update	Cleaning up after installation.
+2018-02-24T09:05:23+00:00	INFO ::1	update	Update to version 3.8.5 is complete.
